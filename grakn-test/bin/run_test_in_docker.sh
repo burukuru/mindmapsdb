a2a5b579c0ac4bf9f5e35be2d8994e2c74c53f5a
Thanh C Pham <thanh@grakn.ai>
2017-01-21 14:00:07 +0000

    Run all tests

--- grakn-test/bin/run_test_in_docker.sh
+++ grakn-test/bin/run_test_in_docker.sh
@@ -3,5 +3,5 @@
 SCRIPT_DIR=$(dirname $0)
 GRAKN_TEST_PROFILE=$1
 
-find "$SCRIPT_DIR"/../../grakn-test/ -name '*Test.java' -o -name '*IT.java' | xargs -n1 basename | sed -e 's/\.java//' | head > /tmp/grakn_mvn_docker_test_list
+find "$SCRIPT_DIR"/../../grakn-test/ -name '*Test.java' -o -name '*IT.java' | xargs -n1 basename | sed -e 's/\.java//' > /tmp/grakn_mvn_docker_test_list
 parallel "/usr/bin/docker run -i -v "$SCRIPT_DIR"/../../grakn-test-profiles/target/surefire-reports:/grakn-src/grakn-test/target/surefire-reports/ -w /grakn-src/ graknlabs/jenkins-with-src-compiled:latest mvn test -DfailIfNoTests=false -Dtest={} -P"${GRAKN_TEST_PROFILE}" -pl grakn-test" < /tmp/grakn_mvn_docker_test_list
