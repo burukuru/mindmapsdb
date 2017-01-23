b3349d340cc51016f8c420806f0ebdfafe44651d
Thanh C Pham <thanh@grakn.ai>
2017-01-23 15:27:45 +0000

    Run tests in other modules too.

--- grakn-test/bin/run_test_in_docker.sh
+++ grakn-test/bin/run_test_in_docker.sh
@@ -3,5 +3,11 @@
 SCRIPT_DIR=$(dirname $0)
 GRAKN_TEST_PROFILE=$1
 
-find "$SCRIPT_DIR"/../../grakn-test/ -name '*Test.java' -o -name '*IT.java' | xargs -n1 basename | sed -e 's/\.java//' > /tmp/grakn_mvn_docker_test_list
-parallel "/usr/bin/docker run -i -v "$SCRIPT_DIR"/../../grakn-test/target/surefire-reports:/grakn-src/grakn-test/target/surefire-reports/ -w /grakn-src/ graknlabs/jenkins-with-src-compiled:latest mvn test -DfailIfNoTests=false -Dtest={} -P"${GRAKN_TEST_PROFILE}" -pl grakn-test" < /tmp/grakn_mvn_docker_test_list
+echo "" > /tmp/grakn_mvn_docker_test_list
+for MODULE_DIR in $(find "$SCRIPT_DIR"/../../* -maxdepth 0 -type d)
+do
+  MODULE=$(basename $MODULE_DIR)
+  find $MODULE_DIR -name '*Test.java' -o -name '*IT.java' | xargs --no-run-if-empty -n1 basename | sed -e "s#^#$MODULE #" -e 's#\.java##' >> /tmp/grakn_mvn_docker_test_list
+done
+parallel --no-run-if-empty --colsep ' ' "echo /usr/bin/docker run -i -v "$SCRIPT_DIR"/../../grakn-test/target/surefire-reports:/grakn-src/grakn-test/target/surefire-reports/ -w /grakn-src/ graknlabs/jenkins-with-src-compiled:latest mvn test -DfailIfNoTests=false -Dtest={1} -P"${GRAKN_TEST_PROFILE}" -pl {2}" < /tmp/grakn_mvn_docker_test_list
+rm /tmp/grakn_mvn_docker_test_list
