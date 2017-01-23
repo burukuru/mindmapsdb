09ac9f88b665970e170f35959ce872a22b3a3f98
Thanh C Pham <thanh@grakn.ai>
2017-01-23 19:56:44 +0000

    Revert "Run tests in other modules too."
    This reverts commit b3349d340cc51016f8c420806f0ebdfafe44651d.

--- grakn-test/bin/run_test_in_docker.sh
+++ grakn-test/bin/run_test_in_docker.sh
@@ -3,11 +3,5 @@
 SCRIPT_DIR=$(dirname $0)
 GRAKN_TEST_PROFILE=$1
 
-echo "" > /tmp/grakn_mvn_docker_test_list
-for MODULE_DIR in $(find "$SCRIPT_DIR"/../../* -maxdepth 0 -type d)
-do
-  MODULE=$(basename $MODULE_DIR)
-  find $MODULE_DIR -name '*Test.java' -o -name '*IT.java' | xargs --no-run-if-empty -n1 basename | sed -e "s#^#$MODULE #" -e 's#\.java##' >> /tmp/grakn_mvn_docker_test_list
-done
-parallel --jobs 75% --no-run-if-empty --colsep ' ' "/usr/bin/docker run -i -v "$SCRIPT_DIR"/../../grakn-test/target/surefire-reports:/grakn-src/grakn-test/target/surefire-reports/ -w /grakn-src/ graknlabs/jenkins-with-src-compiled:latest mvn test -DfailIfNoTests=false -Dsurefire.rerunFailingTestsCount=2 -pl {1} -Dtest={2} -P"${GRAKN_TEST_PROFILE}"" < /tmp/grakn_mvn_docker_test_list
-rm /tmp/grakn_mvn_docker_test_list
+find "$SCRIPT_DIR"/../../grakn-test/ -name '*Test.java' -o -name '*IT.java' | xargs --no-run-if-empty -n1 basename | sed -e 's/\.java//' > /tmp/grakn_mvn_docker_test_list
+parallel --jobs 75% --no-run-if-empty "/usr/bin/docker run -i -v "$SCRIPT_DIR"/../../grakn-test/target/surefire-reports:/grakn-src/grakn-test/target/surefire-reports/ -w /grakn-src/ graknlabs/jenkins-with-src-compiled:latest mvn test -DfailIfNoTests=false -Dtest={} -P"${GRAKN_TEST_PROFILE}" -pl grakn-test" < /tmp/grakn_mvn_docker_test_list
